770df034f53b6823908e6548faf0d2048ec5270d
Milana Cap <milanazzap@gmail.com>
2020-05-28 21:54:07 +0200

    WP-CLI - Building block in theme.

--- wp-content/themes/twentytwenty/functions.php
+++ wp-content/themes/twentytwenty/functions.php
@@ -758,3 +758,6 @@ function twentytwenty_get_elements_array() {
 	*/
 	return apply_filters( 'twentytwenty_get_elements_array', $elements );
 }
+
+// WP-CLI Block
+include_once get_template_directory() . '/blocks/wpcli-block.php';
